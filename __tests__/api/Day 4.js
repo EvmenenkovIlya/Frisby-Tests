d91ed3607fad23f929c1d410c2cb39c3d84632bc
Ilya <95778915+EvmenenkovIlya@users.noreply.github.com>
2022-04-08 13:58:31 +0300

    Update Day 4.js

--- __tests__/api/Day 4.js
+++ __tests__/api/Day 4.js
@@ -1,6 +1,6 @@
 const frisby = require("frisby");
   const Joi = frisby.Joi;
-  const secretKey = 'PMAK-6230f643cc4adf66c7dbe790-e8d2a30e4a7deeb678075b8e329da3cb8a'
+  const secretKey = 'PMAK-6230f643cc4adf66c7dbe790'
   describe("Day4", () => {
     it('Check status', function () {
         return frisby.fetch('https://api.getpostman.com/collections', {
