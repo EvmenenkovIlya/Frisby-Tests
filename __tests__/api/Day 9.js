83e15d4f1769f5f37c1fbace58b5804ed9ad768e
Ilya <Evmenya@gmail.com>
2022-04-19 22:50:43 +0300

    ;;

--- __tests__/api/Day 9.js
+++ __tests__/api/Day 9.js
@@ -3,7 +3,7 @@ const Joi = frisby.joi;
 describe('Day 9', ()=>{
     it('Check status',  function () {
         const gender = 'male'
-        return frisby.post('https://postman-echo.com/post?' + gander, {
+        return frisby.post('https://postman-echo.com/post?' + gender, {
         name: 'Mr Thomas Jones',
         email: 'thomas.jones@example.com',
         uuid: 'e7a58de3-2aae-4aad-b324-7178bcadd158'
